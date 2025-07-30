3b207b6277befb3d37a94c8c3ce5abc1cbffd0f9
arkinn47 <81554660+arkinn47@users.noreply.github.com>
2025-07-30 19:08:09 +0200

    Update edited_mtp.c
    Added comments for more comprehension.

--- MTP_PC/edited_mtp.c
+++ MTP_PC/edited_mtp.c
@@ -7,6 +7,7 @@
 #define BUFFER_SIZE 50000
 #define THREAD_NUM 4
 
+//Buffer 1 variables
 char buffer_1[BUFFER_SIZE]; // Buffer 1, shared resource between input thread and square-root thread
 int count_1 = 0; // Number of items in the buffer 1
 int prod_idx_1 = 0;  // Index where the input thread will put the next item
@@ -15,15 +16,15 @@ pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;  // Initialize the mutex fo
 pthread_cond_t full_1 = PTHREAD_COND_INITIALIZER; // Initialize the condition variable for buffer 1
 char output[5] = {0};
 
-
+//Buffer 2 variables
 char buffer_2[BUFFER_SIZE]; // Buffer 2, shared resource between square root thread and output thread
 int count_2 = 0;  // Number of items in the buffer 2
 int prod_idx_2 = 0;  // Index where the space-line thread will put the next item
 int con_idx_2 = 0;  // Index where the plus-sign thread will pick up the next item
 pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;  // Initialize the mutex for buffer 2
 pthread_cond_t full_2 = PTHREAD_COND_INITIALIZER;  // Initialize the condition variable for buffer 2
-//char thread_1_character;
 
+//Buffer 3 variables
 char buffer_3[BUFFER_SIZE];
 int count_3 = 0; // Number of items in the buffer 1
 int prod_idx_3 = 0;  // Index where the input thread will put the next item
@@ -31,7 +32,7 @@ int con_idx_3 = 0;  // Index where the space-line break thread will pick up the 
 pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;  // Initialize the mutex for buffer 1
 pthread_cond_t full_3 = PTHREAD_COND_INITIALIZER; // Initialize the condition variable for buffer 1
 
-//char character_store[1];
+//Intialized variables
 char stop[6] = {0};
 int started = 0;
 int signaler = 0;
@@ -49,6 +50,8 @@ char item_ps[1] = {'a'};
 
 char items_ot[81];
 
+//Thread 1
+//Function for input and buffer 1 in thread 1
 void put_buffer_1(char character_1){
       pthread_mutex_lock(&mutex_1);
       buffer_1[prod_idx_1] = character_1;
@@ -56,7 +59,6 @@ void put_buffer_1(char character_1){
       count_1++;
       pthread_cond_signal(&full_1);
       pthread_mutex_unlock(&mutex_1);}
-
 
 void *get_input(){
    size_t len = 0;
@@ -69,17 +71,10 @@ while(1){
       for(int i = 0; i < strlen(character_store); i++) {put_buffer_1(character_store[i]);}
              }
       free(character_store);
-              return NULL;
-      }                                                    
+              return NULL;}                                                    
 
-void put_buffer_2(char character_2){
-      pthread_mutex_lock(&mutex_2);
-      buffer_2[prod_idx_2] = character_2;
-      prod_idx_2 = prod_idx_2 + 1;
-      count_2++;
-      pthread_cond_signal(&full_2);
-      pthread_mutex_unlock(&mutex_2);}
-
+//Thread 2
+//Function for taking from buffer 1, filling buffer 2 and line sperating function
 char get_buffer_1(){
       pthread_mutex_lock(&mutex_1);
       while(count_1 == 0)
@@ -89,6 +84,14 @@ char get_buffer_1(){
       count_1--;
       pthread_mutex_unlock(&mutex_1);
       return item;}
+
+void put_buffer_2(char character_2){
+      pthread_mutex_lock(&mutex_2);
+      buffer_2[prod_idx_2] = character_2;
+      prod_idx_2 = prod_idx_2 + 1;
+      count_2++;
+      pthread_cond_signal(&full_2);
+      pthread_mutex_unlock(&mutex_2);}
 
 void *line_seperator(){  
   while(1){ 
@@ -106,6 +109,8 @@ void *line_seperator(){
    }
   return NULL;}
 
+//Thread 3
+//Taking from buffer 2, putting to buffer 3, and 
 char get_buffer_2(){
       pthread_mutex_lock(&mutex_2);
       while(count_2 == 0)
@@ -115,7 +120,6 @@ char get_buffer_2(){
       count_2--;
       pthread_mutex_unlock(&mutex_2);
       return item;}
-
 
 void put_buffer_3(char character_3){
       pthread_mutex_lock(&mutex_3);
@@ -128,7 +132,6 @@ void put_buffer_3(char character_3){
 void *plus_sign(){
       while(1){
       item_ps[0] = get_buffer_2();
-
 
       if(item_ps[0] == '\n'){
           put_buffer_3('\n');
@@ -149,6 +152,8 @@ void *plus_sign(){
     return NULL;
       }
 
+//Thread 4 functions
+//Get from buffer 3 and output 
 char get_buffer_3(){
       pthread_mutex_lock(&mutex_3);
       while(count_3 == 0)
@@ -167,7 +172,6 @@ void *output_thread(){
      items_ot[printed_characters%80] = item_ot[0];
      printed_characters++;
      if(printed_characters%80 == 0 && printed_characters > 0){
-
      write(STDOUT_FILENO, items_ot, 81);
      }
       if(item_ot[0] == '\n'){
@@ -179,19 +183,19 @@ return NULL;}
 
 
 int main(){pthread_t th0, th1, th2, th3;
-    //Create threads
+//Create threads
     pthread_create(&th0,NULL, get_input, NULL);
     pthread_create(&th1,NULL, line_seperator, NULL);
     pthread_create(&th2,NULL, plus_sign, NULL);
     pthread_create(&th3,NULL, output_thread, NULL);
 
-    //Join threads
+//Join threads
     pthread_join(th0, NULL);
     pthread_join(th1, NULL);
     pthread_join(th2, NULL);
     pthread_join(th3, NULL);
 
-    //Destroy threads
+//Destroy threads
     pthread_mutex_destroy(&mutex_1);
     pthread_cond_destroy(&full_1);
 
